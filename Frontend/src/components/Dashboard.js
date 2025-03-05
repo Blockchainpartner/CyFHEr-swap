b2891ee253f966685b8dcff9c9f1264798fc3490
name <58445913+Hazem-dh@users.noreply.github.com>
2025-03-05 15:56:34 +0100

    added logos

--- Frontend/src/components/Dashboard.js
+++ Frontend/src/components/Dashboard.js
@@ -3,22 +3,23 @@ import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headless
 import clsx from 'clsx'
 import { CheckIcon, ChevronDownIcon } from '@heroicons/react/20/solid'
 import { FhenixClient } from 'fhenixjs';
-
-import { TOKEN_CONTRACT } from '../constants/contracts';
-import { ERC_CONTRACT_ABI } from '../ABI/FHERC20ABI';
 import { ethers } from 'ethers';
 import toast from 'react-hot-toast';
+import { TOKEN_CONTRACT } from '../constants/contracts';
+import { ERC_CONTRACT_ABI } from '../ABI/FHERC20ABI';
+import privateEuroLogo from '../assets/private euro.png'
+import privateUsdLogo from '../assets/private usd.png'
+import privateBgpLogo from '../assets/private bgp.png'
+
 
 function Dashboard() {
     const tokens = [
-        { id: 1, name: 'pEUR' },
-        { id: 2, name: 'pUSD' },
-        { id: 3, name: 'pGBP' },
-
+        { id: 1, name: 'pEUR', logo: privateEuroLogo },
+        { id: 2, name: 'pUSD', logo: privateUsdLogo },
+        { id: 3, name: 'pGBP', logo: privateBgpLogo },
     ]
 
-    const [selectedToken, setSelectedToken] = useState('pEUR'); // Default token as EUR
-
+    const [selectedToken, setSelectedToken] = useState(tokens[0]); // Default token as EUR
     const [balance, setBalance] = useState(null);
 
     const handleCheckBalance = async () => {
@@ -28,7 +29,7 @@ function Dashboard() {
         console.log(provider)
 
         console.log(selectedToken)
-        const contractAddress = TOKEN_CONTRACT[selectedToken];
+        const contractAddress = TOKEN_CONTRACT[selectedToken.name];
         const contractABI = ERC_CONTRACT_ABI;
         const contract = new ethers.Contract(contractAddress, contractABI, signer);
         // TODO : to fix error handling
@@ -58,28 +59,25 @@ function Dashboard() {
     const OnChangeToken = (value) => {
         if (selectedToken !== value) {
             setBalance(null);
-
             setSelectedToken(value);
         }
     }
-    return (
-        <div className="md:w-3/5 w-full bg-black text-white  px-5 py-6 rounded-xl shadow-lg  relative border border-purple-800 
-        before:absolute before:inset-0 before:rounded-xl before:blur-lg before:bg-purple-600 before:opacity-50 before:-z-10">
+
+    return (<>
+        <div className="md:w-3/5 w-full bg-black text-white px-5 py-6 rounded-xl shadow-lg relative border border-purple-800 before:absolute before:inset-0 before:rounded-xl before:blur-lg before:bg-purple-600 before:opacity-50 before:-z-10">
             <h1 className="text-2xl font-semibold text-white text-center">Token Balance</h1>
-            <div className="flex  items-center space-x-4">
-                {/* Stylish Token Selector */}
+            <div className="flex items-center space-x-4">
+                {/* Token Selector with Logo */}
                 <Listbox value={selectedToken} onChange={OnChangeToken}>
                     <ListboxButton
                         className={clsx(
-                            'relative block w-2/5 rounded-lg bg-black border border-purple-800 mt-6 py-2 pr-8 pl-4 text-left text-sm/6 text-white',
+                            'relative flex items-center space-x-2 w-2/5 rounded-lg bg-black border border-purple-800 mt-6 py-2 pr-8 pl-4 text-left text-sm/6 text-white',
                             'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black'
                         )}
                     >
-                        {selectedToken}
-                        <ChevronDownIcon
-                            className="group pointer-events-none absolute top-2.5 right-2.5 size-4 fill-white/60"
-                            aria-hidden="true"
-                        />
+                        <img src={selectedToken.logo} alt={selectedToken.name} className="w-6 h-6" />
+                        <span>{selectedToken.name}</span>
+                        <ChevronDownIcon className="absolute top-2.5 right-2.5 size-4 fill-white/60" aria-hidden="true" />
                     </ListboxButton>
                     <ListboxOptions
                         anchor="bottom"
@@ -92,11 +90,12 @@ function Dashboard() {
                         {tokens.map((token) => (
                             <ListboxOption
                                 key={token.id}
-                                value={token.name}
-                                className="group flex cursor-default items-center gap-2 rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
+                                value={token}
+                                className="group flex items-center gap-2 cursor-default rounded-lg py-1.5 px-3 select-none data-[focus]:bg-white/10"
                             >
+                                <img src={token.logo} alt={token.name} className="w-6 h-6" />
+                                <span className="text-sm/6 text-white">{token.name}</span>
                                 <CheckIcon className="invisible size-4 fill-white group-data-[selected]:visible" />
-                                <div className="text-sm/6 text-white">{token.name}</div>
                             </ListboxOption>
                         ))}
                     </ListboxOptions>
@@ -111,15 +110,17 @@ function Dashboard() {
                 </button>
             </div>
 
-
             {/* Display Balance */}
             {balance !== null && (
                 <div className="text-center text-white text-lg mt-4">
                     <p>Your balance is:</p>
-                    <p className="font-bold text-2xl">{balance + " " + selectedToken}</p>
+                    <p className="font-bold text-2xl">{balance + " " + selectedToken.name}</p>
                 </div>
             )}
         </div>
+        { /* TODO : Fetch available pair contracts  and show them */}
+
+    </>
 
 
 
